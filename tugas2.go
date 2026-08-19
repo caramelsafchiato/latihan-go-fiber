755b5f6e5d2b8af37712e04144157f7ad169607d
msi <alisyaaninda@gmail.com>
2026-08-20 01:10:29 +0700

    tambah map data mhs dan operasinya

--- tugas2.go
+++ tugas2.go
@@ -15,4 +15,29 @@ func main() {
 	fmt.Printf("IPK: %.2f\n", ipk)
 	fmt.Printf("Status Aktif: %t\n", isAktif)
 	fmt.Printf("Hobi: %v\n\n", hobi)
+
+	//map data mhs
+	nilaiMahasiswa := make(map[string]int)
+
+	nilaiMahasiswa["Lailia"] = 92
+	nilaiMahasiswa["Valerina"] = 88
+	nilaiMahasiswa["Alisya"] = 93
+	fmt.Println("=== Operasi Map Data Mahasiswa ===")
+	fmt.Println("Data mahasiswa berhasil ditambahkan.")
+
+	namaCari := "Alisya"
+	if nilai, ada := nilaiMahasiswa[namaCari]; ada {
+		fmt.Printf("Data ditemukan -> %s: %d\n", namaCari, nilai)
+	} else {
+		fmt.Printf("Data %s tidak ditemukan.\n", namaCari)
+	}
+
+	fmt.Println("\nMenghapus data Valerina dari map...")
+	delete(nilaiMahasiswa, "Valerina")
+
+	fmt.Println("\nSeluruh data mahasiswa yang tersisa:")
+	for namaMhs, nilai := range nilaiMahasiswa {
+		fmt.Printf("- %s: %d\n", namaMhs, nilai)
+	}
 }
+
